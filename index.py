eb3ef71dbbfd65ce736387094f09d6a84b325072
Anthony Geronimo <159731639+anthonygeronim0@users.noreply.github.com>
2024-03-18 18:30:54 -0300

    Adicionado comentário

--- index.py
+++ index.py
@@ -2,9 +2,11 @@ import pywhatkit
 import pyautogui
 import keyboard
 
-listNumber = ['+5579998336721', '+5579998173662']
+#DENTRO DAS AS ASPAS DUPLAS, ADICONA O NÚMERO NO QUAL DESEJA ENVIAR A MENSAGEM.
+listNumber = ['', '']
 
 for number in listNumber:
-    pywhatkit.sendwhats_image(number, "C:/Users/antho/OneDrive/Área de Trabalho/receba", 'Você recebeu a inteligência do BOT criado por Anthony Geronimo. Agora seu QI aumentou 200%.')
+    #DENTRO DAS ASPAS DUPLAS, ADICIONA A MENSAGEM QUE DESEJA ENVIAR
+    pywhatkit.sendwhatmsg_i(number, '')
     pyautogui.click(1050, 950)
     keyboard.press_and_release('enter')
